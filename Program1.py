f98b18ab3b5e13856b6f87e0ab9cf6d82acfcbc7
unknown <CarloLim01>
2021-10-24 11:33:47 +0800

    First commit

---
+++ Program1.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ") 
+age = input("Enter your age: ")
+address = input("Enter your address: ")
+print(Hi, my name is "+ name + "." " I am " + age + " years old and i live in " + address + ".")
