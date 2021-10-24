403b49a6fd46168da2b0c2e40d69f17313e72b2b
Albert Carlo P. Lim <92630367+CarloLim01@users.noreply.github.com>
2021-10-24 11:31:18 +0800

    Delete Program1.py

--- Program1.py
+++
@@ -1,4 +0,0 @@
-name = input("Enter your name: ") 
-age = input("Enter your age: ")
-address = input("Enter your address: ")
-print(Hi, my name is "+ name + "." " I am " + age + " years old and i live in " + address + ".")
